430d0e63d589cb0acd1c37a7a245cd6cd2d0c65d
Gaurav <gauravghude2512@gmail.com>
2026-07-03 00:14:57 +0530

    Fix CSS and update Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/portfolio-media/',
+    plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
